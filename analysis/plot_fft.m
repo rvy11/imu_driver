29295e8bf84a8743bb8fb9a698065c561968153f
Robin Yohannan <yohannan.r@husky.neu.edu>
2020-02-09 16:10:13 -0500

    Checkpoint

---
+++ analysis/plot_fft.m
@@ -0,0 +1,11 @@
+% Plot and return FFT of signal
+function fft_out = plot_fft(X, Fs, L)
+    fig = figure;
+    Y = fft(X);
+    P2 = abs(Y/L);
+    P1 = P2(1:L/2+1);
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(L/2))/L;
+    plot(f,P1); 
+    fft_out = Y;
+end
